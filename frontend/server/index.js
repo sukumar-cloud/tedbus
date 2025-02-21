7b7588e84efae8a6b22770699f756cf87ab4b683
sukumar <sukumaramaravathi@gmail.com>
2025-02-21 16:49:16 +0530

    modified index js

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -2,6 +2,7 @@ const express=require('express')
 const bodyparser=require('body-parser')
 const cors =require('cors')
 const mongoose=require('mongoose')
+const admin = require("./firebase");
 
 const app=express();
 
@@ -27,3 +28,22 @@ const PORT= 5000
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
 })
+app.post("/send-notification", async (req, res) => {
+    const { title, body, token } = req.body;
+
+    const message = {
+        notification: {
+            title: title,
+            body: body,
+        },
+        token: token,
+    };
+
+    try {
+        await admin.messaging().send(message);
+        res.status(200).send("Notification sent successfully!");
+    } catch (error) {
+        console.error("Error sending notification:", error);
+        res.status(500).send("Failed to send notification");
+    }
+});
